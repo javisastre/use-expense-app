3bc0b905bfcb0fc42d4e5d2111911747b8a4a986
Javi Sastre <javisastrecal@gmail.com>
2021-04-01 13:56:07 +0200

    App.js implemented

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import React from "react";
+import { Switch, Route } from "react-router-dom";
 import "./App.css";
 
-function App() {
-  return <div className='App'></div>;
-}
+import Navbar from "./Components/Navbar";
+import AddExpense from "./Components/AddExpense";
+import Activity from "./Components/Activity";
+import EditExpense from "./Components/EditExpense";
+import Overview from "./Components/Overview";
+import Timeline from "./Components/Timeline";
+
+const App = () => {
+  return (
+    <div className='App'>
+      <Navbar />
+
+      <Switch>
+        <Route exact path='/' component={AddExpense} />
+        <Route exact path='/activity' component={Activity} />
+        <Route exact path='/expense' component={EditExpense} />
+        <Route exact path='/overview' component={Overview} />
+        <Route exact path='/timeline' component={Timeline} />
+      </Switch>
+    </div>
+  );
+};
 
 export default App;
---
+++ src/Components/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { useState } from "react";
+
+const Navbar = () => {
+  const [display, setDisplay] = useState(false);
+
+  return <div></div>;
+};
+
+export default Navbar;
