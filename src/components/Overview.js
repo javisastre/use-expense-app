d83d0d89f43954789fc04c351b0be23adcc1592e
Javi Sastre <javisastrecal@gmail.com>
2021-04-14 19:36:46 +0200

    Month implementation on Overview v1

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -1,51 +1,91 @@
 import React, { useState, useEffect } from "react";
 import { VictoryPie } from "victory";
 
+import { makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    "& > *": {
+      margin: theme.spacing(1),
+    },
+  },
+  title: {
+    marginTop: "4vh",
+    fontWeight: "600",
+    textAlign: "center",
+  },
+  salary: {
+    backgroundColor: "#14B0F3",
+    color: "white",
+    marginTop: "2vh",
+    textAlign: "center",
+  },
+}));
+
 const Overview = ({ activities }) => {
-  const [categories, setCategories] = useState({
-    bar: 0,
-    restaurant: 0,
-    grocery: 0,
-    transport: 0,
-  });
+  const [bar, setBar] = useState(1);
+  const [restaurant, setRestaurant] = useState(1);
+  const [grocery, setGrocery] = useState(1);
+  const [transport, setTransport] = useState(1);
+  const [salary, setSalary] = useState(0);
+  const [currentMonth, setCurrentMonth] = useState(3);
+
+  const classes = useStyles();
 
   useEffect(() => {
-    activities.forEach((act) => {
+    const monthActivities = activities.filter((activity) => {
+      return new Date(activity.created_at).getMonth() === currentMonth;
+    });
+
+    monthActivities.forEach((act) => {
       switch (act.category) {
         case "Bar/Cafeteria":
-          const cat1 = categories.bar + act.amount;
-          setCategories({ ...categories, bar: cat1 });
+          let newBar = bar + act.amount;
+          setBar(newBar);
           break;
         case "Restaurant":
-          const cat2 = categories.restaurant + act.amount;
-          setCategories({ ...categories, restaurant: cat2 });
+          setRestaurant(restaurant + act.amount);
           break;
         case "Grocery Store":
-          const cat3 = categories.grocery + act.amount;
-          setCategories({ ...categories, grocery: cat3 });
+          setGrocery(grocery + act.amount);
           break;
         case "Transport":
-          const cat4 = categories.transport + act.amount;
-          setCategories({ ...categories, transport: cat4 });
+          setTransport(transport + act.amount);
           break;
+        case "Salary":
+          setSalary(salary + act.amount);
         default:
           break;
       }
     });
-  }, []);
 
-  console.log(categories);
+    console.log("activities", activities);
+    console.log("monthActivities", monthActivities);
+  }, [currentMonth]);
 
   return (
     <div>
+      <Typography variant='h5' className={classes.title}>
+        Current Month Expenses
+      </Typography>
+
       <VictoryPie
+        animate={{ duration: 500 }}
+        colorScale={["#0066DD", "#01AC83", "#DF0086", "#E7A600"]}
         data={[
-          { x: "Bar/Cafeteria", y: categories.bar },
-          { x: "Restaurant", y: categories.restaurant },
-          { x: "Grocery Store", y: categories.grocery },
-          { x: "Transport", y: categories.transport },
+          { x: `Bar/Cafeteria: ${bar}`, y: bar },
+          { x: `Restaurant: ${restaurant}`, y: restaurant },
+          { x: `Grocery Store: ${grocery}`, y: grocery },
+          { x: `Transport: ${transport}`, y: transport },
         ]}
       />
+      <Typography variant='h5' className={classes.title}>
+        Current Month Income
+      </Typography>
+      <Typography variant='h5' className={classes.salary}>
+        {salary}
+      </Typography>
     </div>
   );
 };
