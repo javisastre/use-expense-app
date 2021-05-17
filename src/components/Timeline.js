6b75f940620d8a1720ee789353d54401e1088779
Javi Sastre <javisastrecal@gmail.com>
2021-05-17 17:03:41 +0200

    finished timeline implementation

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,9 +1,51 @@
 import React, { useState, useEffect } from "react";
+
 import { VictoryChart, VictoryGroup, VictoryBar } from "victory";
+
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    "& > *": {
+      margin: theme.spacing(1),
+    },
+  },
+  title: {
+    marginTop: "4vh",
+    fontWeight: "600",
+    textAlign: "center",
+  },
+  income: {
+    backgroundColor: "#01AC83",
+    color: "white",
+    marginTop: "2vh",
+    paddingTop: "2vh",
+    paddingBottom: "2vh",
+    textAlign: "center",
+    width: "80vw",
+  },
+  expense: {
+    backgroundColor: "#ff340f",
+    color: "white",
+    marginTop: "2vh",
+    paddingTop: "2vh",
+    paddingBottom: "2vh",
+    textAlign: "center",
+    width: "80vw",
+  },
+  centerAll: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+}));
 
 const Timeline = ({ activities, monthList, monthConverter }) => {
   const [incomeData, setIncomeData] = useState([]);
   const [expenseData, setExpenseData] = useState([]);
+
+  const classes = useStyles();
 
   useEffect(() => {
     const setValues = () => {
@@ -15,45 +57,69 @@ const Timeline = ({ activities, monthList, monthConverter }) => {
         };
         return obj;
       });
-      const exp = [...inc];
+      const exp = reverseMonthList.map((mo) => {
+        let obj = {
+          x: monthConverter(mo),
+          y: 0,
+        };
+        return obj;
+      });
 
-      activities.map((element) => {});
-
+      activities.forEach((element) => {
+        const monthNumber = new Date(element.created_at).getMonth();
+        if (element.isIncome) {
+          exp[monthNumber].y += element.amount;
+        } else {
+          inc[monthNumber].y += element.amount;
+        }
+      });
       setIncomeData(inc);
       setExpenseData(exp);
     };
     setValues();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
-    <div>
+    <div className={classes.centerAll}>
+      <Typography variant='h5' className={classes.title}>
+        All time expenses & incomes
+      </Typography>
       <VictoryChart domainPadding={50}>
         <VictoryGroup
           height={3200}
-          horizontal={true}
           offset={24}
-          colorScale={["tomato", "#01AC83"]}
+          colorScale={["#ff340f", "#01AC83"]}
           animate={{
             duration: 500,
             onLoad: { duration: 100 },
           }}
         >
-          <VictoryBar
-            data={[
-              { x: "april", y: 3 },
-              { x: "may", y: 3 },
-              { x: "june", y: 3 },
-            ]}
-          />
-          <VictoryBar
-            data={[
-              { x: "april", y: 2 },
-              { x: "may", y: 1 },
-              { x: "june", y: 7 },
-            ]}
-          />
+          <VictoryBar data={incomeData} />
+          <VictoryBar data={expenseData} />
         </VictoryGroup>
       </VictoryChart>
+      <Typography variant='h6' className={classes.expense}>
+        Total expenses:{" "}
+        {incomeData.map((elem) => {
+          return (
+            <Typography variant='overline' display='block'>
+              {elem.x}: {elem.y}
+            </Typography>
+          );
+        })}
+      </Typography>
+
+      <Typography variant='h6' className={classes.income}>
+        Total incomes:{" "}
+        {expenseData.map((elem) => {
+          return (
+            <Typography variant='overline' display='block'>
+              {elem.x}: {elem.y}
+            </Typography>
+          );
+        })}
+      </Typography>
     </div>
   );
 };
